9b45e9da6054e9d9006dfd679f000c365b6d8d3f
Danielle <goncalvesdani54@hotmail.com>
2023-06-06 19:36:26 -0300

    script banco de dados

---
+++ script.sql
@@ -0,0 +1,10 @@
+---Script para usar no MySQL e criar o banco de dados e tabela para o projeto---
+
+create database nomes_flask;
+
+USE nomes_flask;
+
+CREATE TABLE nomes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
